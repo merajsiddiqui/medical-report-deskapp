1d6a452893d7ebff2b0e65063a6e5ad6978aa1a9
Afzal Nomani <afzalnomani1@gmail.com>
2022-10-23 16:04:23 +0530

    implemented add button functionality

--- renderer.js
+++ renderer.js
@@ -1,1 +1,17 @@
-console.log("rendered");
+const addBtn = document.querySelector(".add");
+
+addBtn.addEventListener("click", function () {
+  const html = `<div class="pre">
+    <select>
+      <option>Tablet</option>
+      <option>Inject.</option>
+      <option>Syrup</option>
+    </select>
+    <input type="text" placeHolder="Name">
+    <input type="text" placeholder="Dose">
+    <input type="number" placeholder="Quantity">
+  </div>`;
+
+  const pres = document.querySelector(".prescription");
+  pres.insertAdjacentHTML("afterbegin", html);
+});
